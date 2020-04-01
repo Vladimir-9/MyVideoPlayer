4de614e3f6d6918611cc80f19664a7ba5f1e83e6
Vladimir Sakaev <vovchik-5@bk.ru>
2020-04-02 00:57:41 +0500

    initial commit

---
+++ app/src/main/java/com/example/myvideoplayer/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.myvideoplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int intPosition;
+    private VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        videoView = findViewById(R.id.videoView);
+        String pathVideoFile = "android.resource://com.example.myvideoplayer/" + R.raw.videoplayback;
+        videoView.setVideoPath(pathVideoFile);
+
+        if (savedInstanceState != null) {
+            intPosition = savedInstanceState.getInt("intPosition");
+            videoView.seekTo(intPosition);
+            videoView.start();
+        } else {
+            videoView.start();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        videoView.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        videoView.seekTo(intPosition);
+        videoView.start();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        videoView.pause();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        videoView.seekTo(intPosition);
+        videoView.start();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        intPosition = videoView.getCurrentPosition();
+        outState.putInt("intPosition", intPosition);
+    }
+}
